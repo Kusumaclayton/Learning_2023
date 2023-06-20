6d9324a0bc916c72d5d27e30df96df3e66aefa34
unknown <kusumaclayton@gmail.com>
2023-06-20 22:26:57 +0530

    Module1 is commited

---
+++ Module1/Day5/question1.c
@@ -0,0 +1,37 @@
+/* 1. Use structures to find the volume and the total surface area of the box. 
+   You need to access the members of the structure with the help of a structure pointer:
+    a. With the help of (*) asterisk or indirection operator and (.) dot operator.
+    b. With the help of ( -> ) Arrow operator using pointer as well as dot representation.*/
+
+    #include <stdio.h>
+struct Box {
+    double length;
+    double width;
+    double height;
+};
+double calculateVolume(struct Box* boxPtr) {
+    return boxPtr->length * boxPtr->width * boxPtr->height;
+}
+double calculateSurfaceArea(struct Box* boxPtr) {
+    return 2 * (boxPtr->length * boxPtr->width + boxPtr->length * boxPtr->height + boxPtr->width * boxPtr->height);
+}
+int main() {
+    struct Box myBox;
+    struct Box* boxPtr = &myBox;
+    myBox.length = 5.0;
+    myBox.width = 3.0;
+    myBox.height = 4.0;
+    printf("Using (*) and (.) operators:\n");
+    printf("Length: %.2f\n", (*boxPtr).length);
+    printf("Width: %.2f\n", (*boxPtr).width);
+    printf("Height: %.2f\n", (*boxPtr).height);
+    printf("\nUsing (->) operator:\n");
+    printf("Length: %.2f\n", boxPtr->length);
+    printf("Width: %.2f\n", boxPtr->width);
+    printf("Height: %.2f\n", boxPtr->height);
+    double volume = calculateVolume(boxPtr);
+    double surfaceArea = calculateSurfaceArea(boxPtr);
+    printf("\nVolume: %.2f\n", volume);
+    printf("Surface Area: %.2f\n", surfaceArea);
+    return 0;
+}
